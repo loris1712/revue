c399db739160eb6555e385253150c64317d9e7ca
Loris Caputo <46825967+loris1712@users.noreply.github.com>
2024-12-06 14:57:02 +0100

    Changes

--- src/app/layout.server.js
+++ src/app/layout.server.js
@@ -7,7 +7,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/logo.svg" type="image/x-icon" />
+        <link rel="icon" href="/revue_small_logo.png" type="image/x-icon" />
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap"
           rel="stylesheet"
